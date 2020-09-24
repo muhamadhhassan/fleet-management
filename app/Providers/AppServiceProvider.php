31d0a1e53a9f780f7acf7f250fc5b553482f6a48
muhamadhhassan <muhamadhhassan@gmail.com>
2020-09-24 22:18:12 +0200

    feat: Add custom blade if directives and register ide helper service in local environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        /**
+         * This service provider help the ide recognize the facades.
+         */
+        if ($this->app->isLocal()) {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
     }
 
     /**
@@ -23,6 +29,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        /**
+         * Check if the authenticated user is admin
+         */
+        Blade::if('admin', function () {
+            return optional(auth()->user())->is_admin;
+        });
+
+        /**
+         * Check if the authenticated user is a customer
+         */
+        Blade::if('customer', function () {
+            return auth()->check() && !auth()->user()->is_admin;
+        });
     }
 }
