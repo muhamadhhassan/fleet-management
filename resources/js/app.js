bc7a67153bd36b732f91d1798fec35fd1521c457
muhamadhhassan <muhamadhhassan@gmail.com>
2020-09-24 22:26:24 +0200

    feat: add front-end assets

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
+import Vue from 'vue';
+import StopsInput from './components/StopsInput.vue';
+
 require('./bootstrap');
+
+
+const app = new Vue({
+  el: '#app',
+  components: {
+    StopsInput,
+  },
+});
