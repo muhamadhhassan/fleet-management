4156b5e726dcfddc092b56c5ee8056ddbc4d5103
muhamadhhassan <muhamadhhassan@gmail.com>
2020-09-25 01:24:16 +0200

    feat: add args validator

--- app/GraphQL/Mutations/ReservationMutator.php
+++ app/GraphQL/Mutations/ReservationMutator.php
@@ -2,15 +2,18 @@
 
 namespace App\GraphQL\Mutations;
 
-use App\Exceptions\NoReservationException;
-use App\Http\Resources\ReservationResource;
 use App\Models\City;
 use App\Services\ReservationService;
+use App\Exceptions\ValidationException;
+use Illuminate\Support\Facades\Validator;
+use App\Exceptions\NoReservationException;
+use App\Http\Resources\ReservationResource;
 
 class ReservationMutator
 {
     public function create($_, $args)
     {
+        $this->validateArgs($args);
         $departureCity = City::find($args['departureCityId']);
         $arrivalCity = City::find($args['arrivalCityId']);
         $reservationService = new ReservationService($departureCity, $arrivalCity);
@@ -27,5 +30,20 @@ class ReservationMutator
         ]);
         
         return new ReservationResource($reservation);
+    }
+
+    protected function validateArgs($args)
+    {
+        $validator = Validator::make($args, [
+            'departureCityId' => 'required|exists:cities,id|different:arrivalCityId',
+            'arrivalCityId' => 'required|exists:cities,id|different:departureCityId',
+        ], [
+            'departureCityId.different' => 'A city can be added once',
+            'arrivalCityId.different' => 'A city can be added once',
+        ]);
+
+        if ($validator->fails()) {
+            throw new ValidationException('The given data was invalid', $validator->errors()->messages());
+        }
     }
 }
